a95c600da48ab04454e6028ddfc3fc95688c4341
ashimjunior12 <87907533+ashimjunior12@users.noreply.github.com>
2022-12-31 14:03:41 +0545

    Update mp3_downloader.py

--- mp3_downloader.py
+++ mp3_downloader.py
@@ -1,26 +1,10 @@
-'''
-DOCSTRING
-
-STEPS to run the script:
-- pip install bs4
-- pip install selenium
-- pip install requests
-- download chromedriver and add it to environment path variables
-- other steps
-'''
-
-
-# pip install beautifulsoup4
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup                       # pip install beautifulsoup4
 from time import sleep
 from selenium import webdriver                      # pip install selenium
-# to enter key strokes and submit button
-from selenium.webdriver.common.keys import Keys
-import os
-import requests
-import warnings                       # pip install requests
+from selenium.webdriver.common.keys import Keys     # to enter key strokes and submit button
+import os, requests, warnings                       # pip install requests
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 mp3_website = 'https://ytmp3.nu/'
 
 try:
@@ -33,39 +17,37 @@ try:
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    driver = webdriver.Chrome(options=options)
-    driver.get(url(search_query))
+    driver = webdriver.Chrome(options=options)          # Specify the executable path of the chromedriver
+    driver.get(url(search_query))                 
 
     def song_url(content):
         soup = BeautifulSoup(content, 'lxml')
-        link = soup.find('a', {'id': 'video-title'})
-        video_url = f"https://www.youtube.com/{link.get('href')}"
+        link = soup.find('a', {'id':'video-title'})        
+        video_url = f"https://www.youtube.com/{link.get('href')}"      
         return video_url
 
-    content_of_youtube = driver.page_source.encode('utf-8').strip()
+    content_of_youtube = driver.page_source.encode('utf-8').strip()      
 except:
     pass
 
 try:
     def main(url):
         driver.get(url)
-        driver.find_element_by_xpath(
-            './/*[@id="url"]').send_keys(song_url(content_of_youtube))
+        driver.find_element_by_xpath('.//*[@id="url"]').send_keys(song_url(content_of_youtube))
         driver.find_element_by_xpath('.//*[@id="form"]/form/input[3]').click()
         sleep(4)
 
         content_of_mp3_website = driver.page_source.encode('utf-8').strip()
         sleep(1)
         soup = BeautifulSoup(content_of_mp3_website, 'lxml')
-        download_link = soup.find('div', {'id': 'download'})
+        download_link = soup.find('div', {'id':'download'})
         song_download = download_link.find('a', href=True)
         content_to_be_download = song_download['href']
-        resoponse = requests.get(content_to_be_download)
+        resoponse = requests.get(content_to_be_download)        
         driver.quit()
         print('File being downloaded....')
 
-        # Specify the path where you want to download.
-        os.chdir('C:\\Users\\User_name\\Downloads')
+        os.chdir('C:\\Users\\Aadarsha Bhattarai\\Downloads')
 
         with open(f'{search_query}.mp3', 'wb') as file:
             for chunk in resoponse.iter_content():
@@ -79,3 +61,9 @@ try:
 except:
     driver.quit()
     print("Note: Please check your internet connection!!")
+
+
+
+
+
+
